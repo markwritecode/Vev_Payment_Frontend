cd87264816cd6e1f1d187f2afa0f22b42b1fe0fb
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2022-03-31 11:44:14 +0100

    quick ui fix

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -5,7 +5,7 @@ import LeftDashboard from '../components/Transactions/LeftDashboard'
 const Transactions = () => {
     return (
         <Layout>
-            <div className='h-full w-full space-y-8 lg:px-10' id='Invoice-Page'>
+            <div className='h-full w-full space-y-8 px-10 py-10' id='Invoice-Page'>
                 <h3 className='text-2xl font-semibold'>My wallet</h3>
                 <div className='grid grid-cols-1 lg:grid-cols-3 lg:gap-10'>
                     <div className='lg:col-span-1 space-y-6'>
