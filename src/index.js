9da0712da025282ef43421069ce7a2c2cac82467
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2022-04-17 13:27:35 +0100

    limit antd design css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'antd/dist/antd.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import 'antd/lib/avatar/style/index.css'
+import 'antd/lib/drawer/style/index.css'
+import 'antd/lib/modal/style/index.css'
+import 'antd/lib/message/style/index.css'
+import 'antd/lib/notification/style/index.css'
+import 'antd/lib/form/style/index.css'
+import 'antd/lib/input/style/index.css'
+import 'antd/lib/input-number/style/index.css'
+import 'antd/lib/select/style/index.css'
+import 'antd/lib/dropdown/style/index.css'
+import 'antd/lib/button/style/index.css'
+import 'antd/lib/menu/style/index.css'
+import 'antd/lib/popconfirm/style/index.css'
+import 'antd/lib/table/style/index.css'
+import 'antd/lib/style/index.css'
 import './index.css'
 
 ReactDOM.render(
