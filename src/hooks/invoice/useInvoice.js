5d474e18fa01ac5aeaccead7c00d35def9176619
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2022-04-21 17:32:15 +0100

    Bearer token added to requests

--- src/hooks/invoice/useInvoice.js
+++ src/hooks/invoice/useInvoice.js
@@ -8,11 +8,13 @@ export const liveAxios = axios.create({
 });
 
 export const poster = async (url, { data } = { data: {} }) => {
+    liveAxios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('ichor-token-key')}`
     const response = await liveAxios.post(url, data)
     return response;
 }
 
 export const getter = async url => {
+    liveAxios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('ichor-token-key')}`
     const response = await liveAxios.get(url)
     return response;
 }
