1d50d69c0d7981c884b4458db8b8e7eaf22a4a6b
Timilehin Omotugba <omotugbatimilehin7@gmail.com>
2022-08-22 14:35:13 +0100

    approve payments

--- src/components/ApproveMoney/index.js
+++ src/components/ApproveMoney/index.js
@@ -1,16 +1,43 @@
-import { Dropdown, Menu } from 'antd'
+import { Dropdown, Menu, Modal } from 'antd'
 import { BsThreeDotsVertical } from 'react-icons/bs'
+import { usePoster } from '../../hooks/poster'
+import { useFetchLocalStorageData } from '../../hooks/utilities/useFetchLocalStorage'
+import { currencyFormatter, formatDateAndTime2 } from '../../utils/helperFunctions'
 
-const index = ({ data }) => {
+const ApproveMoney = ({ data, loading }) => {
 
-    const menu = (
-        <Menu
-            items={[
-                { label: 'Approve', key: 'approve' },
-                { label: 'Reject', key: 'reject' }
-            ]}
-        />
-    )
+    const { user } = useFetchLocalStorageData()
+    const { mutate, isLoading } = usePoster('authorize/payment', 'Payment Approved', ['payments'])
+
+    function menu(item) {
+        return (
+            <Menu
+                items={[
+                    ...item.owner === user.email ? [{ label: <span onClick={() => confirmationModal(item)}>Approve</span>, key: 'approve' }] : [],
+                    // { label: 'Reject', key: 'reject' }
+                ]}
+            />
+        )
+    }
+
+    const imgs = [
+        'https://images.unsplash.com/photo-1552788960-65fcafe071a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1285&q=80',
+        'https://images.unsplash.com/photo-1496086363406-27fbdabb8593?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2706&q=80',
+        'https://images.unsplash.com/photo-1616640045164-deb3b104c4b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+        'https://images.unsplash.com/photo-1615921511258-0aa98c84d400?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+        'https://images.unsplash.com/photo-1615719413546-198b25453f85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1336&q=80'
+    ]
+
+    function confirmationModal(data) {
+        Modal.confirm({
+            title: `Confirmation required`,
+            content: `Approve payment of ${currencyFormatter(data.amount)} Naira for ${data.recipient}`,
+            centered: true,
+            confirmLoading: isLoading,
+            cancelText: 'Cancel',
+            onOk: () => mutate({ payment_reference: data.transaction_reference })
+        })
+    }
 
     return (
         <div className='overflow-x-auto'>
@@ -32,21 +59,21 @@ const index = ({ data }) => {
                                 <tr tabIndex={index} key={index} className='focus:outline-none h-[83px] border-y border-black border-opacity-20 text-base font-semibold text-black text-opacity-60'>
                                     <td className=''>
                                         <div className='flex items-center gap-3'>
-                                            <img alt='vev' className='h-12 w-12 rounded' src={item.img} />
+                                            <img alt='vev' className='h-12 w-12 rounded' src={imgs[index]} />
                                             <div>
-                                                <p>{item.name}</p>
-                                                <p className='text-sm font-normal'>{item.description}</p>
+                                                <p>{item.owner === user.email ? item.recipient : item.owner}</p>
+                                                <p className='text-sm font-normal'>{item.transaction_reference}</p>
                                             </div>
                                         </div>
                                     </td>
-                                    <td className='pl-24'>{item.date}</td>
-                                    <td className='pl-5'>{item.payment}</td>
+                                    <td className='pl-24'>{formatDateAndTime2(item.created_at)}</td>
+                                    <td className='pl-5 capitalize'>{item.payment_method}</td>
                                     <td className='pl-5'>
                                         <button className={`border rounded-[10px] p-[5px] text-xs font-medium capitalize ${item.status === 'pending' ? 'text-[#FC8906] border-[#FC8906]' : item.status === 'paid' ? 'text-[#0A9807] border-[#0A9807]' : 'text-[#FF0D0D] border-[#FF0D0D]'}`}>{item.status}</button>
                                     </td>
-                                    <td className='pl-5'>{item.amount}</td>
+                                    <td className='pl-5'>{currencyFormatter(item.amount)}</td>
                                     <td className='pl-4'>
-                                        <Dropdown overlay={menu} trigger={['click']}>
+                                        <Dropdown placement='topRight' overlay={() => menu(item)} trigger={['click']}>
                                             <BsThreeDotsVertical className='w-5 h-5 cursor-pointer' />
                                         </Dropdown>
                                     </td>
@@ -56,8 +83,9 @@ const index = ({ data }) => {
                     }
                 </tbody>
             </table>
+            {loading && 'Loading...'}
         </div>
     )
 }
 
-export default index
+export default ApproveMoney
--- src/pages/ApproveMoney.js
+++ src/pages/ApproveMoney.js
@@ -1,17 +1,14 @@
 import { Tabs } from 'antd'
+import { useFetcher } from '../hooks/fetcher'
 import ApproveMoneyComp from '../components/ApproveMoney'
 
 const { TabPane } = Tabs
 
 const ApproveMoney = () => {
 
-    const data = [
-        { img: 'https://images.unsplash.com/photo-1552788960-65fcafe071a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1285&q=80', name: 'Kendrick Babies', description: 'Sandals 4pc', date: '12/8/2022 9:30 Pm', payment: 'PayPal', status: 'pending', amount: '$500.00' },
-        { img: 'https://images.unsplash.com/photo-1496086363406-27fbdabb8593?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2706&q=80', name: 'Romania Facials', description: 'Facial Cream 2pc', date: '12/8/2022 9:30 Pm', payment: 'Vev', status: 'paid', amount: '$500.00' },
-        { img: 'https://images.unsplash.com/photo-1616640045164-deb3b104c4b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80', name: 'Dior cloths', description: 'Facial Cream 2pc', date: '12/8/2022 9:30 Pm', payment: 'Master Card', status: 'pending', amount: '$500.00' },
-        { img: 'https://images.unsplash.com/photo-1615921511258-0aa98c84d400?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80', name: 'Burger King', description: 'Facial Cream 2pc', date: '12/8/2022 9:30 Pm', payment: 'Visa', status: 'failed', amount: '$500.00' },
-        { img: 'https://images.unsplash.com/photo-1615719413546-198b25453f85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1336&q=80', name: 'Dominos Pizza', description: 'Facial Cream 2pc', date: '12/8/2022 9:30 Pm', payment: 'PayPal', status: 'pending', amount: '$500.00' }
-    ]
+    const { data, isLoading } = useFetcher('payments')
+    // const { data: payments } = useFetcher('authorize/payment')
+    console.log(data)
 
     return (
         <div className='sm:px-6 pt-[60px] md:pt-[35px] w-full bg-white h-full'>
@@ -21,10 +18,10 @@ const ApproveMoney = () => {
             <div className='bg-white py-4 md:py-7 px-4 md:px-8 xl:px-10'>
                 <Tabs defaultActiveKey='all'>
                     <TabPane tab='All' key='all'>
-                        <ApproveMoneyComp data={data} />
+                        <ApproveMoneyComp data={data?.payments} loading={isLoading} />
                     </TabPane>
                     <TabPane tab='Sent' key='sent'>
-                        <ApproveMoneyComp data={data?.filter(item => item.status === 'paid')} />
+                        <ApproveMoneyComp data={data?.payments?.filter(item => item.status === 'paid')} loading={isLoading} />
                     </TabPane>
                 </Tabs>
             </div>
